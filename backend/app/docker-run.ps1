5075754b8100b48914e282f283d39fab6bd52a6c
Bartek Łukasik <blukasik@cybervadis-lab.com>
2018-02-19 10:56:47 +0100

    SQL port opened in the app container.
    Added template docker-compose file.

--- backend/app/docker-run.ps1
+++ backend/app/docker-run.ps1
@@ -1,4 +1,4 @@
-docker stop devops-app-api-v0.1
-docker rm devops-app-api-v0.1
-docker run -d -p 8080:80 --name devops-app-api-v0.1 devops-app-api
+docker stop devops-app-api-ps
+docker rm devops-app-api-ps
+docker run -d -p 8080:80 -p 1433:1433 --name devops-app-api-ps devops-app-api
 docker ps
